a6a3b7af34cb9e7602d0d6f9652a1237fbb7e75c
aminehann <amine.hanyny.91@gmail.com>
2023-12-15 20:57:08 +0100

    Dec 15 20:56

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,35 @@
 /*=============== SHOW MENU ===============*/
-const navToggle = document.getElementById('nav-toggle');
-const navClose = document.getElementById('nav-close')
-const navMenu = document.getElementById('nav-menu');
-const navLink = document.querySelectorAll('.nav__link');
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+const navMenu = document.getElementById("nav-menu");
+const navLink = document.querySelectorAll(".nav__link");
 
-navToggle.addEventListener('click',showMenu);
-navClose.addEventListener('click',closeMenu);
+navToggle.addEventListener("click", showMenu);
+navClose.addEventListener("click", closeMenu);
 
-function showMenu () {navMenu.classList.toggle('show__menu')}
-function closeMenu () {navMenu.classList.remove('show__menu')}
+function showMenu() {
+  navMenu.classList.toggle("show__menu");
+}
+function closeMenu() {
+  navMenu.classList.remove("show__menu");
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
-function linkAction (){
-    navMenu.classList.remove('show__menu');
+function linkAction() {
+  navMenu.classList.remove("show__menu");
 }
-navLink.forEach(element => {element.addEventListener('click',linkAction);});
+navLink.forEach((element) => {
+  element.addEventListener("click", linkAction);
+});
 
 /*=============== SHADOW HEADER ===============*/
 
-
 /*=============== EMAIL JS ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
