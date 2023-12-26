5f092f03d53e87ae8e32c1c747664acca6895fb8
aminehann <amine.hanyny.91@gmail.com>
2023-12-26 20:07:37 +0100

    Dec 26 20:07

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,6 +25,30 @@ navLink.forEach((element) => {
 /*=============== SHADOW HEADER ===============*/
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form");
+const contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+  e.preventDefault();
+  // serviceID - templateID - #form - publicKey
+  emailjs.sendForm("service_uv2ijtc", "template_muut40h", "#contact-form", "omm1uabw3MowNuE8F")
+    .then(() => {
+      // Show sent message
+      contactMessage.textContent = 'Message sent successfully ✅';
+      // Remove message after five seconds
+      setTimeout(() => {
+        contactMessage.textContent = '';
+      }, 5000);
+    })
+    .catch((error) => {
+      // Handle errors
+      console.error('Error sending email:', error);
+      // Display an error message to the user if needed
+      contactMessage.textContent = 'Error sending message ❌';
+    });
+};
+
+contactForm.addEventListener('submit', sendEmail);
 
 /*=============== SHOW SCROLL UP ===============*/
 
